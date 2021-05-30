ecf75e3c3d8f531143bc708f446b2568f54d0a16
Tangjj1996 <tangjj_1996@163.com>
2021-05-30 16:19:25 +0800

    feat: differant replace mothod

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,18 +1,31 @@
-number_2_words = {
-    '0': 'zero',
-    '1': 'one',
-    '2': 'two',
-    '3': 'three',
-    '4': 'four',
-    '5': 'five',
-    '6': 'six',
-    '7': 'seven',
-    '8': 'eight',
-    '9': 'nine'
-}
+# number_2_words = {
+#     '0': 'zero',
+#     '1': 'one',
+#     '2': 'two',
+#     '3': 'three',
+#     '4': 'four',
+#     '5': 'five',
+#     '6': 'six',
+#     '7': 'seven',
+#     '8': 'eight',
+#     '9': 'nine'
+# }
 
-input_val = input('This is an amazing game, Please enter your keywords: ')
-new_val = ''
-for key in input_val:
-    new_val += number_2_words.get(key) + ' '
-print(new_val)
+# input_val = input('This is an amazing game, Please enter your keywords: ')
+# new_val = ''
+# for key in input_val:
+#     new_val += number_2_words.get(key) + ' '
+# print(new_val)
+
+######
+# advance usage
+input_val = input('let your comment here: ')
+word_group = input_val.split(' ')
+emoji_map = {
+    ':)': '😃',
+    ':(': '🙁'
+}
+out_str = ''
+for word in word_group:
+    out_str += emoji_map.get(word, word) + ' '
+print(out_str)
